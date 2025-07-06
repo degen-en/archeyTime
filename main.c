c2cab64f103974693402786fd59668e18e88c210
degen-en <degenvodka@gmail.com>
2025-07-06 23:21:37 +0300

    Update main.c
    Making prettier

--- main.c
+++ main.c
@@ -4,14 +4,12 @@
 void showTime(long timeMinus, time_t seconds,char style);
 void convert(time_t seconds);
 
-
 // Define time units with floating-point precision
 const double DROP = 4.0;                                // 1 drop ≈ 4 seconds (0.00021428571L)
 const double KIAF = 210.0 * DROP;                       // 1 kiaf ≈ 14 minutes (0.045L)
 const double SKIFOS = 1845 * DROP;                      // 1 skifos ≈ 2 hours 3 minutes (0.395L)
 const double KARAS = 24300 * DROP;                      // 1 karas ≈ 27 hours (5.20714L)
 const double PITHOS = 190.6996931136862078 * KARAS;     // 1 pithos ≈ 18536010.17065029939816 seconds (993L)   993 - you know.
-
 
 long pithoses;
 long karases;
@@ -22,11 +20,9 @@ long drops;
 
 int main() {
     
-    
     // Get current UNIX time
     time_t seconds = time(NULL);
 
-   
                             // printf("\033[93mLight Yellow Text\033[0m\n");
     char stroke[] = "\033[93m=========================================================||\033[0m";
     printf("%s", stroke);
@@ -43,8 +39,6 @@ int main() {
     return 0;
 }
 
-
-
 void convert(time_t seconds)
 {
     pithoses = (long)(seconds / PITHOS);
@@ -53,26 +47,12 @@ void convert(time_t seconds)
     kiafs = (long)((seconds - pithoses * PITHOS - karases * KARAS - skifoses * SKIFOS) / KIAF);
     drops = (long)((seconds - pithoses * PITHOS - karases * KARAS - skifoses * SKIFOS - kiafs * KIAF) / DROP);
 }
+
 void showTime(long timeMinus, time_t seconds, char style)
 {
-
-
     convert(seconds - timeMinus); //- converts
     printf("\033[%dm%ld pithoses, %ld karases, %ld skifoses, %ld kiafs, %ld drops.\033[0m\n", style, pithoses, karases, skifoses, kiafs, drops); //- prints
 
 
 }
 
-
-
-    /*The code is pretty straightforward. It defines the time units and then calculates the number of each unit since January 1, 1970. 
-    The code is compiled with the following command: 
-    "gcc main.c -o exe" in core folder
-    
-    And then executed with: 
-    "./exe" in folder
-    
-    The output is: 
-    Since January 1, 1970:
-    */
-    
